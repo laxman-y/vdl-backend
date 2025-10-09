cfe4b519a8366cdf458bed9a5d8f02315568c04b
laxman-y <laxmanyadav.dev@gmail.com>
2025-10-09 12:12:18 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -38,6 +38,7 @@ const studentSchema = new mongoose.Schema({
   admissionDate: Date,
   password: String,
   attendance: [attendanceSchema],
+   isActive: { type: Boolean, default: true }, // ✅ New Field
   fees: [feeSchema],
   modificationHistory: [modificationSchema] // 🔧 New field added here
 });
