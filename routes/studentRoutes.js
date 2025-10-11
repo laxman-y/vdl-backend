a42f7a54b92a87db7e1e188fc66413569eda5243
laxman-y <laxmanyadav.dev@gmail.com>
2025-10-11 17:44:07 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -401,7 +401,7 @@ doc.lineWidth(1)
 
     // === Watermark ===
   // import path from "path";
-const watermarkPath = path.join(__dirname, "../public/logo.png");
+const watermarkPath = path.join(__dirname, "../public/logo1.png");
 if (fs.existsSync(watermarkPath)) {
   doc.image(watermarkPath, doc.page.width / 2 - 100, doc.page.height / 2 - 100, {
     width: 200,
