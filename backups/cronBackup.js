cdf89b799bfe810d5782300f1fb54a1eced4edf3
laxman-y <laxmanyadav.dev@gmail.com>
2026-06-18 15:26:51 +0530

    Update cronBackup.js

--- backups/cronBackup.js
+++ backups/cronBackup.js
@@ -1,12 +1,5 @@
-const cron = require("node-cron");
-
-cron.schedule("* * * * *", () => {
-  console.log("=================================");
-  console.log("Running Daily Backup...");
-  console.log("=================================");
-
-  delete require.cache[require.resolve("./backupAll")];
-  require("./backupAll");
+cron.schedule("0 2 * * *", () => {
+    console.log("Running Daily Backup...");
+    delete require.cache[require.resolve("./backupAll")];
+    require("./backupAll");
 });
-
-console.log("✅ Daily Backup Scheduler Started");
